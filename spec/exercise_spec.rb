f899e290e8c7d886f4bde0d0907435a06e03d9e7
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-10-11 12:08:50 -0700

    broken string

---
+++ spec/exercise_spec.rb
@@ -0,0 +1,8 @@
+describe "String" do
+  context "substitution" do
+    it "should substitute 'message' for 'function'" do
+      message = "One function can be confused with another function."
+      message.sub('function', 'message').should eq "One message can be confused with another message."
+    end
+  end
+end
